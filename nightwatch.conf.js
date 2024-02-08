2c2aeb96641301d816e4049a568ab76175a80a6f
Gary Behan <gary.b@browserstack.com>
2024-02-08 12:10:47 +0000

    Initial Commit

---
+++ nightwatch.conf.js
@@ -0,0 +1,114 @@
+const additonalEnvironments = require("./environments");
+
+if(!additonalEnvironments.test_settings)
+  additonalEnvironments.test_settings = {};
+
+const bstackOptions = {
+  'bstack:options' : {
+    "os" : "OS X",
+    "osVersion" : "Big Sur",
+    "buildName" : "browserstack-build-1",
+    "sessionName" : "BStack nightwatch snippet",
+    "source": "nightwatch:sample-sdk:v1.0",
+    "seleniumVersion" : "4.0.0",
+    userName: '${BROWSERSTACK_USERNAME}' || 'YOUR_USERNAME',
+    accessKey: '${BROWSERSTACK_ACCESS_KEY}' || 'YOUR_ACCESS_KEY',
+  },
+}
+
+const browserStack = {
+  webdriver: {
+    start_process: false,
+    timeout_options: {
+      timeout: 120000,
+      retry_attempts: 3
+    },
+    keep_alive: true
+  },
+
+  selenium: {
+    host: 'hub.browserstack.com',
+    port: 443
+  },
+
+  desiredCapabilities: {
+    browserName: 'chrome',
+    ...bstackOptions
+  }
+}
+
+const nightwatchConfigs = {
+  src_folders: [],
+  live_output: true,
+  plugins: ['@nightwatch/browserstack'],
+  '@nightwatch/browserstack': {
+    browserstackLocal: true // set true to manage browserstack local tunnel. Defaults to false.
+  },
+
+  test_settings: {
+    default: {
+      launch_url: 'https://nightwatchjs.org'
+    },
+
+    chrome: {
+      desiredCapabilities: {
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+          // More info on Chromedriver: https://sites.google.com/a/chromium.org/chromedriver/
+          //
+          // w3c:false tells Chromedriver to run using the legacy JSONWire protocol (not required in Chrome 78)
+          w3c: true,
+          args: [
+            //'--no-sandbox',
+            //'--ignore-certificate-errors',
+            //'--allow-insecure-localhost',
+            //'--headless'
+          ]
+        }
+      },
+
+      webdriver: {
+        start_process: true,
+        server_path: '',
+        cli_args: [
+          // --verbose
+        ]
+      }
+    },
+
+    browserstack:  {
+      ...browserStack
+    },
+
+    "browserstack.chrome": {
+      ...browserStack,
+      desiredCapabilities:{
+        browserName: 'chrome',
+        ...bstackOptions
+      }
+    },
+    "browserstack.firefox": {
+      ...browserStack,
+      desiredCapabilities:{
+        browserName: 'firefox',
+        ...bstackOptions
+      }
+    },
+    "browserstack.edge": {
+      ...browserStack,
+      desiredCapabilities:{
+        browserName: 'Edge',
+        ...bstackOptions
+      }
+    }
+  }
+}
+
+for(let key in additonalEnvironments.test_settings) {
+  nightwatchConfigs.test_settings[key] = {
+    ...browserStack,
+    ...additonalEnvironments.test_settings[key]
+  };
+}
+
+module.exports = nightwatchConfigs;
---
+++ tests/login-test.js
@@ -0,0 +1,84 @@
+
+describe('Practice Login Tests', function () {
+    beforeEach(function(done){
+        browser.navigateTo('https://practicetestautomation.com/practice-test-login/');
+    });
+
+    it('Perform a Successful Login', function (browser) {
+        var username = "student";
+        var password = "Password123";
+        
+        browser
+            .waitForElementVisible({
+            selector: '#username',
+            timeout: 10000,
+            retryInterval: 200
+        })
+        .sendKeys('#username', [username])
+        .waitForElementVisible({
+            selector: '#password',
+            timeout: 10000,
+            retryInterval: 200
+        })
+        .sendKeys('#password', [password])
+        .waitForElementVisible({
+            selector: '#submit',
+            timeout: 10000,
+            retryInterval: 200
+        })
+        .click('#submit')
+        .assert.urlContains('logged-in-successfully');
+    });
+
+    it('Failed Login based on username', function (browser) {
+        var wrongUsername = "wrongStudent";
+        var correctPassword = "Password123";
+
+        browser
+            .waitForElementVisible({
+            selector: '#username',
+            timeout: 10000,
+            retryInterval: 200
+        })
+            .sendKeys('#username', [wrongUsername])
+            .waitForElementVisible({
+            selector: '#password',
+            timeout: 10000,
+            retryInterval: 200
+        })
+            .sendKeys('#password', [correctPassword])
+            .waitForElementVisible({
+            selector: '#submit',
+            timeout: 10000,
+            retryInterval: 200
+        })
+            .click('#submit')
+            .assert.textContains('#error', 'Your username is invalid!');
+    });
+    
+    it('Failed Login based on password', function (browser) {
+        var correctUsername = "student";
+        var wrongPassword = "Password321";
+
+        browser
+            .waitForElementVisible({
+            selector: '#username',
+            timeout: 10000,
+            retryInterval: 200
+        })
+            .sendKeys('#username', [correctUsername])
+            .waitForElementVisible({
+            selector: '#password',
+            timeout: 10000,
+            retryInterval: 200
+        })
+            .sendKeys('#password', [wrongPassword])
+            .waitForElementVisible({
+            selector: '#submit',
+            timeout: 10000,
+            retryInterval: 200
+        })
+            .click('#submit')
+            .assert.textContains('#error', 'Your password is invalid!');
+    });
+});
